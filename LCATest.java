ccd17c241110d7d15876d262cbdeee51b37ae4f4
C-Nev <conevin@tcd.ie>
2017-10-17 16:11:49 +0100

    Binary Tree Tests 3
    Some formatting and size() test

--- LCATest.java
+++ LCATest.java
@@ -4,48 +4,52 @@ import org.junit.Test;
 
 public class LCATest {
 
-	
-	/*Will need to test all Binary Tree Functions
-	 * Empty 
-	 * Print -Done
-	 * Insert
-	 * Get
-	 * Delete
-	 *Then need to test LCA functions
+	/*
+	 * Will need to test all Binary Tree Functions Empty -Done Print -Done
+	 * Insert Get Delete Then need to test LCA functions
 	 *
 	 */
-	
+
 	@Test
-	public void testEmptyTree()
-	{
-		//Test isEmpty Function
+	public void testEmptyTree() {
+		// Test isEmpty Function
 		BinaryTree<Character, Character> BT = new BinaryTree<Character, Character>();
 		assertTrue(BT.isEmpty());
 		BT.insert('a', 'a');
 		assertFalse(BT.isEmpty());
 	}
+
 	@Test
-	public void testPrintTree()
-	{
-		//Test empty tree printing
-	     BinaryTree<Character, Character> BT = new BinaryTree<Character, Character>();
-	     assertEquals("Empty tree test print",
-	             "()", BT.printTree());
-	     
-	     //Testing one node tree printing
-	     BT.insert('a', 'a');
-	     assertEquals("One node tree test print",
-	             "(()a())", BT.printTree());
-	     
-	     
-	     //Testing multiple node tree printing
-	     BT.insert('w', 'w');
-	     BT.insert('x', 'x');
-	     BT.insert('y', 'y');
-	     BT.insert('z', 'z');
-	     
-	     assertEquals("Many node test print",
-	    		 "(()a(()w(()x(()y(()z())))))", BT.printTree());	     
+	public void testSize() {
+		// Test Size of Tree Function
+		BinaryTree<Character, Character> BT = new BinaryTree<Character, Character>();
+		assertEquals(0, BT.size());
+		BT.insert('a', 'a');
+		assertEquals(1, BT.size());
+		BT.insert('w', 'w');
+		BT.insert('x', 'x');
+		BT.insert('y', 'y');
+		BT.insert('z', 'z');
+		assertEquals(5, BT.size());
 	}
-	
+
+	@Test
+	public void testPrintTree() {
+		// Test empty tree printing
+		BinaryTree<Character, Character> BT = new BinaryTree<Character, Character>();
+		assertEquals("Empty tree test print", "()", BT.printTree());
+
+		// Testing one node tree printing
+		BT.insert('a', 'a');
+		assertEquals("One node tree test print", "(()a())", BT.printTree());
+
+		// Testing multiple node tree printing
+		BT.insert('w', 'w');
+		BT.insert('x', 'x');
+		BT.insert('y', 'y');
+		BT.insert('z', 'z');
+
+		assertEquals("Many node test print", "(()a(()w(()x(()y(()z())))))", BT.printTree());
+	}
+
 }
