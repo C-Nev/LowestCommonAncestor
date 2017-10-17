0fc2faf5cad93bf50daee5e163de520fdd44ace6
C-Nev <conevin@tcd.ie>
2017-10-17 13:35:55 +0100

    LcaTest2
    Commit with first "valid" LCA test.
    
    At least now it isn't junk.

---
+++ LCATest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LCATest {
+
+	@Test
+    public void checkBTNull(){     
+        assertEquals(Entity.count, 0);
+    }
+	
+}
